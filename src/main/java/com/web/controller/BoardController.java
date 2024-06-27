ad2f6065400410e190513d16761e3a6c17124aa4
Dudae <aldlqp123@naver.com>
2024-06-27 17:41:19 +0900

    식품DB API 사용전 저장

--- src/main/java/com/web/controller/BoardController.java
+++ src/main/java/com/web/controller/BoardController.java
@@ -1,7 +1,7 @@
 package com.web.controller;
 
-import com.web.service.BoardService;
 import com.web.domain.Board;
+import com.web.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,21 +9,21 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class BorderController {
+public class BoardController {
 
     @Autowired
     private BoardService boardService;
 
-    @GetMapping("/borderlist")
-    public String borderlist(Model model, Pageable pageable) {
-        if (pageable == null) {
-            pageable = PageRequest.of(0, 8); // 기본 페이지 크기를 8로 설정
-        }
+    @GetMapping("/boardlist")
+    public String boardlist(Model model, @RequestParam(defaultValue = "0") int page) {
+        Pageable pageable = PageRequest.of(page, 8); // 페이지 크기를 8로 설정
         Page<Board> boardPage = boardService.getBoardList(pageable);
         model.addAttribute("boardList", boardPage.getContent());
         model.addAttribute("totalPages", boardPage.getTotalPages());
-        return "border/borderlist";
+        model.addAttribute("currentPage", page);
+        return "border/boardlist";
     }
 }
