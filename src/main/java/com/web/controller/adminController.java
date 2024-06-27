4ba15ba22e33c5077b2282d26230dcc8054ff216
OMG <lalalu0321@gmail.com>
2024-06-27 15:34:21 +0900

    관리자페이지 추가

---
+++ src/main/java/com/web/controller/adminController.java
@@ -0,0 +1,16 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class adminController {
+
+	   // 테스트
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin/admin_main";
+    }
+    
+	
+}
