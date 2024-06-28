e022a60b31472cfc7cea70a56490c03b93c6a5f9
OMG <lalalu0321@gmail.com>
2024-06-28 13:29:59 +0900

    관리자페이지 수정

--- src/main/java/com/web/controller/AdminController.java
+++ src/main/java/com/web/controller/AdminController.java
@@ -2,15 +2,37 @@ package com.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("admin")
 public class AdminController {
 
-	   // 테스트
-    @GetMapping("/admin")
+	// 테스트
+	
+    @GetMapping("main")
     public String admin() {
         return "admin/admin_main";
     }
     
+    @GetMapping("memberList")
+    public String adminMemberList() {
+        return "admin/admin_member";
+    }
+    
+    @GetMapping("boardList")
+    public String adminBoardList() {
+    	return "admin/admin_board";
+    }
+    
+    @GetMapping("noticeList")
+    public String adminNoticeList() {
+    	return "admin/admin_notice";
+    }
+    
+    @GetMapping("inquiryList")
+    public String adminInquiryList() {
+    	return "admin/admin_inquiry";
+    }
 	
 }
