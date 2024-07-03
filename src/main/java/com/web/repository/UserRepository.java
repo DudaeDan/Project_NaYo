dec1e7be8978bb3c33d04d97c1472fec9257cc3c
ahndaa <ahndaaa1230@gamil.com>
2024-07-03 09:15:29 +0900

    로그인,세션,아이디 찾기 구현 완료 아이디찾기css미완료

--- src/main/java/com/web/repository/UserRepository.java
+++ src/main/java/com/web/repository/UserRepository.java
@@ -2,6 +2,44 @@ package com.web.repository;
 
 import com.web.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+// SQL을 편하게 작성해주는 인터페이스
+// 콘솔에 Hibernate를 이용해서 확인 가능하다.
+
+
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserId(String userId);
+        // 유저 아이디를 찾는다.
+    User findByUserNameAndUserPhonenumber(String userName, String userPhonenumber);
+
+    
+
 }
+
+//package com.web.repository;
+//
+//import com.web.domain.User;
+//
+//import java.util.Optional;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//
+//    //로그인
+//	User findByUserIdAndUserPw(String userId, String userPw);
+//
+//
+//
+//	//회원가입 중복 체크
+//	boolean existsByUserId(String userId);
+//	boolean existsByUserNickname(String userNickname);
+//	Optional<User> findByUserId(String userId);
+//	
+//
+//
+//}
--- src/main/java/com/web/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.web.service;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface MemberService {
-
-	public int loginCheck(HttpServletRequest request);
-
-}
