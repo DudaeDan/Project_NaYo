9524f9fe7499f44b407d085c4badcaa84f4c0709
OMG <lalalu0321@gmail.com>
2024-07-12 17:35:39 +0900

    페이지들 css 정리

--- src/main/resources/static/script/layout/Layout_scripts.js
+++ src/main/resources/static/script/layout/Layout_scripts.js
@@ -1,11 +1,8 @@
 function toggleSidebar() {
     var sidebar = document.querySelector('.sidebar');
-    var menuButton = document.querySelector('.menu-button');
     if (sidebar.classList.contains('show')) {
         sidebar.classList.remove('show');
-        menuButton.style.display = 'block';
     } else {
         sidebar.classList.add('show');
-        menuButton.style.display = 'none';
     }
 }
